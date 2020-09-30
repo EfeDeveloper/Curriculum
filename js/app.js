9fa9461ef33713f634681b8d2fc3c3c3f9af50d3
Edwin Ferney Villa T <edwinvilla_adsi@hotmail.com>
2020-09-29 21:14:47 -0500

    ➕ADD: Seccion contador
    Se añade seccion con contador de proyectos, clientes felices, capacitaciones, cursos  & talleres, todo con animacion en JavaScript

--- js/app.js
+++ js/app.js
@@ -4,8 +4,45 @@ $(document).ready(function () {
   bars.forEach(function (bar) {
     let percentage = bar.dataset.percent;
     let tooltip = bar.children[0];
-    tooltip.innerText = percentage + '%';
-    bar.style.width = percentage + '%';
-    console.log(percentage);
+    tooltip.innerText = percentage + "%";
+    bar.style.width = percentage + "%";
   });
+
+  // Contador
+
+  const counters = document.querySelectorAll(".counter");
+
+  function runCounter() {
+    counters.forEach((counter) => {
+      counter.innerText = 0;
+      let target = +counter.dataset.count;
+      let step = target / 100;
+
+      let countIt = function () {
+        let displayedCount = +counter.innerText;
+        if (displayedCount < target) {
+          counter.innerText = Math.ceil(displayedCount + step);
+          setTimeout(countIt, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+      countIt();
+    });
+  }
+
+  let counterSection = document.querySelector(".counter_section");
+  let options = {
+    rootMargin: "0px 0px -200px 0px",
+  };
+  let done = 0;
+
+  const sectionObserver = new IntersectionObserver(function (entries) {
+    if (entries[0].isIntersecting && done !== 1) {
+      done = 1;
+      runCounter();
+    }
+  }, options);
+
+  sectionObserver.observe(counterSection);
 });
